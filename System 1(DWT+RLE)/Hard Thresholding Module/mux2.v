8da25e9387a422ca11063d566d6ea7e7316eadfc
Ibrahim Mostafa <167284854+ibrahim-mostafaa@users.noreply.github.com>
2024-06-24 03:55:31 +0300

    Delete System 1(DWT+RLE) directory

--- System 1(DWT+RLE)/Hard Thresholding Module/mux2.v
+++
@@ -1,8 +0,0 @@
-module mux2 #(parameter width =8 )(
-input  [width-1:0] d0,d1, 
-input  sel, 
-output  [7:0] y);
-
-assign y=sel?d1:d0;
-
-endmodule
--- System 1(DWT+RLE)/RLE/xor.v
+++
@@ -1,6 +0,0 @@
-module xor_1bit (
-input wire a,b,
-output wire y); 
-
-assign y = a^b; 
-endmodule 
