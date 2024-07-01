d712039d208e6d579d608825a5eff5cc18920374
Ibrahim Mostafa <167284854+ibrahim-mostafaa@users.noreply.github.com>
2024-07-01 09:37:17 +0300

    Update full_dct_transform.v

--- System2 (DCT+RLE)_rtl/System2_top(DCT+RLE)_rtl/full_dct_transform.v
+++ System2 (DCT+RLE)_rtl/System2_top(DCT+RLE)_rtl/full_dct_transform.v
@@ -4,7 +4,7 @@
 module full_dct_transform(
 input signed [7:0]input0,input1,input2,input3,input4,input5,input6,input7,
 input clk,rst_n,en,cs, 
-output signed [12:0] integer_Z0, integer_Z1, integer_Z2, integer_Z3, integer_Z4,integer_Z5, integer_Z6, integer_Z7
+output signed [11:0] integer_Z0, integer_Z1, integer_Z2, integer_Z3, integer_Z4,integer_Z5, integer_Z6, integer_Z7
 );
 
 wire signed [18:0] fraction_z0, fraction_z1, fraction_z2,fraction_z3, fraction_z4,fraction_z5, fraction_z6, fraction_z7;
