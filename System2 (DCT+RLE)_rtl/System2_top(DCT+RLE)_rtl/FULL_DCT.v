94474cf90fe813c8f2df8df3066e1a57332065d8
Ibrahim Mostafa <167284854+ibrahim-mostafaa@users.noreply.github.com>
2024-07-01 09:40:30 +0300

    Update FULL_DCT.v

--- System2 (DCT+RLE)_rtl/System2_top(DCT+RLE)_rtl/FULL_DCT.v
+++ System2 (DCT+RLE)_rtl/System2_top(DCT+RLE)_rtl/FULL_DCT.v
@@ -8,19 +8,19 @@ module full_dct(
     
 input signed [7:0]input0,input1,input2,input3,input4,input5,input6,input7,
 input clk,rst_n,en,buf_en,cs, 
-output signed [12:0] Output,[7:0]Counter);
+output signed [11:0] Output,[7:0]Counter);
 
 wire signed[18:0] fraction_z0, fraction_z1, fraction_z2,fraction_z3,
 fraction_z4,fraction_z5, fraction_z6, fraction_z7;
 
-wire signed [12:0] integer_Z0,integer_Z1, integer_Z2, integer_Z3, 
+wire signed [11:0] integer_Z0,integer_Z1, integer_Z2, integer_Z3, 
 integer_Z4,integer_Z5, integer_Z6, integer_Z7;
 
-wire signed [12:0] dout; 
-wire signed [12:0] rle_Input; 
+wire signed [11:0] dout; 
+wire signed [11:0] rle_Input; 
 
-wire signed [12:0] neg_threshold = 12'b111111111011; // negative threshold =-5
-wire signed [12:0] pos_threshold = 12'b000000000101; //  positive threshold = 5
+wire signed [11:0] neg_threshold = 11'b111111111011; // negative threshold =-5
+wire signed [11:0] pos_threshold = 11'b000000000101; //  positive threshold = 5
 
 //dct_top
 dct_top top1(
